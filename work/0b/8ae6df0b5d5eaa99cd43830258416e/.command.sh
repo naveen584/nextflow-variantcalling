3a926e5c42f1b1e409fc73a528d5d29144a8e6ce
lynngroup <lynngroup@mail.com>
2019-11-17 20:09:11 +0530

    First commit

--- work/0b/8ae6df0b5d5eaa99cd43830258416e/.command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ue
-mkdir genomeDir
-cp ebola_ref.fasta genomeDir/genome.fa
-samtools faidx genomeDir/genome.fa
-gatk CreateSequenceDictionary --REFERENCE genomeDir/genome.fa --OUTPUT=genomeDir/genome.dict
-STAR --runMode genomeGenerate --genomeDir genomeDir --genomeFastaFiles genomeDir/genome.fa --genomeSAindexNbases 6 --runThreadN 4
--- work/59/febfa16068cf201d2561f210f13d61/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-mkdir star_rundir
-cd star_rundir
-STAR --genomeDir ../genomeDir/ --readFilesIn ../ebola_mut_reads1.fq ../ebola_mut_reads2.fq --runThreadN 4
